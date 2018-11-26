b463c216a45c85bddd6f5cdffd346af55550ab49
CHOMINJI <>
2018-11-26 12:45:55 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int select;
+        MenuViewer.showMenu();
+        select = MenuViewer.scan.nextInt();
+        MenuViewer.scan.nextLine();
+
+        switch (select) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                System.out.println("Management Program Exit...");
+                break;
+
+        }
+
     }
 }
