c68ee319e4535f37089c58a58530ee7a39c280c3
CHOMINJI <>
2018-11-26 12:46:28 +0900

    Create MenuViewer Class

--- src/MenuViewer.java
+++ src/MenuViewer.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class MenuViewer {
+
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void showMenu() {
+        System.out.println("==== Main Menu ====");
+        System.out.println("1.Contacts");
+        System.out.println("2.To-Do List");
+        System.out.println("3.Appointments");
+        System.out.println("4.Exit");
+        System.out.print("Select: ");
+    }
+
+
 }
