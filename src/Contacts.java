b15c3981c5ab741088c53f3b80f0c2753337099f
sukyoung <rainst127@gmail.com>
2018-11-27 22:38:53 +0900

    Contacts

---
+++ src/Contacts.java
@@ -0,0 +1,28 @@
+
+public class Contacts {
+	private String name;
+	private String phone;
+	private String email;
+	
+	public void selectContacts(int select){
+		switch (select){
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		}
+	
+	}
+	private void createContacts(){
+		
+	}
+	private void viewContacts(){
+		
+	}
+	private void updateContacts(){
+		
+	}
+	private void deleteContacts(){
+		
+	}
+}
