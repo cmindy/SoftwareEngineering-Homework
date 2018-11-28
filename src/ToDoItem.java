d63dd6ecbba521058c1df277b34583bdaa1bc3bf
CHOMINJI <>
2018-11-28 16:03:10 +0900

    Update ToDoItem create ToDoItem, showToDoItem()

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -1,16 +1,2 @@
 public class ToDoItem {
-    String date;
-    String due;
-    String description;
-
-    public ToDoItem(String date, String due, String description) {
-        this.date = date;
-        this.due = due;
-        this.description = description;
-    }
-    public void showToDoItem() {
-        System.out.println("date: " + date);
-        System.out.println("due: " + due);
-        System.out.println("description: " + description);
-    }
 }
