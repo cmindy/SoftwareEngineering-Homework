0cc343ec1aeebe4cc1fdc4639aa2d10938c0a266
sukyoung <rainst127@gmail.com>
2018-11-29 03:25:10 +0900

    switch문

--- src/Contacts.java
+++ src/Contacts.java
@@ -8,9 +8,19 @@ public class Contacts {
 	public void selectContacts(int select){
 		switch (select){
 		case 1:
+			System.out.print("Contacts 정보 입력:");
+			break;
 		case 2:
+			viewContacts();
+			break;
 		case 3:
+			viewContacts();
+			System.out.print("수정할 인덱스 입력:");
+			break;
 		case 4:
+			viewContacts();
+			System.out.print("삭제할 인덱스 입력:");
+			break;
 		}
 	
 	}
